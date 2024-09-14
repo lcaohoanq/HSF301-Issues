4163906f44d4a919f22ef8bdffc98aa218574ea8
lcaohoanq <hoangclw@gmail.com>
2024-09-14 22:12:46 +0700

    feat: add first demo Student Project

--- HSF301_Chaper01_Ex_01_ManyToOne/src/main/java/com/fe/pojo/Student.java
+++ HSF301_Chaper01_Ex_01_ManyToOne/src/main/java/com/fe/pojo/Student.java
@@ -12,10 +12,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
 @Entity
 @Table(name= "students")
 public class Student {
@@ -23,7 +23,7 @@ public class Student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
-	private int id;
+	private Long id;
 	
 	@Column(name = "first_name")
 	private String firstName;
@@ -33,5 +33,55 @@ public class Student {
 	
 	@Column(name = "marks")
 	private int marks;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	
+	public Student(Long id, String firstName, String lastName, int marks) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.marks = marks;
+	}
+
+	public Student(String firstName, String lastName, int marks) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.marks = marks;
+	}
+	
+	public Student() {
+		
+	}
+	
 	
 }
